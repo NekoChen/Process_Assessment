55d2e1dd37f26650e5d791523aaf94c34eb77524
NekoChen <38178384+NekoChen@users.noreply.github.com>
2020-01-29 18:55:34 +0800

    SSM_Branch:SpringMVC环境配置成功

--- src/main/Java/com/hc/controller/test_Controller.java
+++ src/main/Java/com/hc/controller/test_Controller.java
@@ -1,6 +1,14 @@
 package com.hc.controller;
 
-public class test_Controller {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class test_Controller {
+    @RequestMapping("/account/findAll")
+    public String findALl(){
+        System.out.println("Controller:查询所有账户");
+        return "list";
+    }
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -6,5 +6,6 @@
 <body>
 <h2>这是一个抽象的数电过程性考核~</h2>
 <h2>沁怡我爱你</h2>
+<a href="account/findAll">测试SpringMVC查询</a>
 </body>
 </html>
